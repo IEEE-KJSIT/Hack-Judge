84e687d6f508f79268fcf2d301d073687f553aca
Qasim21569 <qasim.k@somaiya.edu>
2025-03-23 11:17:17 +0530

    Frontend update

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,14 +5,13 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAskjNRKaLPC36hkv5gMbMVU1t5_zlV_5U",
-  authDomain: "knowcode-2-round-2.firebaseapp.com",
-  projectId: "knowcode-2-round-2",
-  storageBucket: "knowcode-2-round-2.firebasestorage.app",
-  messagingSenderId: "485340885672",
-  appId: "1:485340885672:web:169f63088c53d1a3fa489d"
+  apiKey: "AIzaSyBETLH6CXLiOLGptIm-REk3EAh73p_VNJM",
+  authDomain: "algoforge-1817d.firebaseapp.com",
+  projectId: "algoforge-1817d",
+  storageBucket: "algoforge-1817d.firebasestorage.app",
+  messagingSenderId: "705860952578",
+  appId: "1:705860952578:web:705c21dc0f0ffd69c1fd1b"
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
